aefa4c1cd699417574756ef9f0b5b02c05ccb859
MemmedzadeAygun <147386696+MemmedzadeAygun@users.noreply.github.com>
2025-04-01 11:34:13 -0700

    create project

---
+++ full-stack-ecommerce/full-stack-ecommerce/src/test/java/az/developia/full_stack_ecommerce/FullStackEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package az.developia.full_stack_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
